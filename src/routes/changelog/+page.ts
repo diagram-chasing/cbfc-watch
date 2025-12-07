446e56bcaac779c057a222926c389caa7ce1876d
Aman Bhargava <amanbhargava2001@gmail.com>
2025-12-07 09:51:03 +0530

    fix: prerender issues

--- src/routes/changelog/+page.ts
+++ src/routes/changelog/+page.ts
@@ -2,6 +2,8 @@
 import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
+export const prerender = false;
+
 export const load: PageLoad = async ({ fetch, url }) => {
   const page = Number(url.searchParams.get('page')) || 1;
 
